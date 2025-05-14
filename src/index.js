6ad799412e4a46d1919e31d5af77ff5639849179
techyVS <victorsky90@gmail.com>
2025-05-14 07:59:09 +0100

    added element transition and animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './scss/main.scss';
+
+const contentWrap = document.querySelector('.content');
+
+window.addEventListener('DOMContentLoaded', () => {
+	if (contentWrap) {
+		setTimeout(() => {
+			contentWrap.classList.add('show-content');
+		}, 300);
+	}
+});
